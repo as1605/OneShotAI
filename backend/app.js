f54a23f49a894760d70f0aaf0189031a40f11a8b
as1605 <1605.aditya.singh@gmail.com>
2022-02-22 22:05:45 +0530

    Heroku Port

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
